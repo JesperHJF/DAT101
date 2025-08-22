0ce574efe7aac5ac50f142cc33b0aed808cb3702
JesperHJF <jhfranck@uia.no>
2025-08-23 00:53:46 +0200

    Added .gitignore and .prettierrc.json

--- module-1/task_1-1/task_1-1.mjs
+++ module-1/task_1-1/task_1-1.mjs
@@ -1,7 +1,7 @@
 "use strict"
 const domTextOut = document.getElementById("txtOut");
 domTextOut.innerHTML = `
-<p>Hello JavaScript version ECMAScript 6!</p>
+<p>Hello JavaScript version ECMAScript 69!</p>
 <p>Successfully running modules in a Live Server environment.</p>
 <p style="text-align: center;">🎉</p> 
 `;
