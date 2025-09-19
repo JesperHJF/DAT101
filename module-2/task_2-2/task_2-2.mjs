6923506cba2b1e3e09612b02135680143585e280
JesperHJF <jhfranck@uia.no>
2025-09-19 15:02:50 +0200

    Completed task 2-2 and 3-1

--- module-2/task_2-2/task_2-2.mjs
+++ module-2/task_2-2/task_2-2.mjs
@@ -4,51 +4,148 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const Answer1 = (2 + 3 * 2 - 4 * 6);
+const Answer1Alt = (2 + 3 * (2 - 4) * 6);
+printOut("The answer to the expression 2 + 3 * 2 - 4 * 6 is: " + Answer1);
+printOut("The answer to the expression 2 + 3 * (2 - 4) * 6 is: " + Answer1Alt);
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+let Meters = 2534000;
+const inches = (Meters / 254 / 10);
+const inchesRounded = Math.round(inches * 100) / 100;
+printOut("25 meters and 34 centimeters equal to " + (inchesRounded) + " inches.");
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const Days = 3;
+const Hours = 12;
+const Minutes = 14;
+const Seconds = 45;
+let DtoM = Days * 24 * 60;
+let HtoM = Hours * 60;
+let MtoM = Minutes;
+let StoM = Seconds / 60;
+let Answer3 = DtoM + HtoM + MtoM + StoM;
+
+printOut("3 days, 12 hours, 14 minutes and 45 seconds equal to " + Answer3 + " minutes.");
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Convert 6,322.52 minutes to days, hours, minutes, and seconds. (Not allowed to use date objects). The
+task must be solved with primitives.*/
+const TotalMinutes = 6322.52;
+const Days2 = TotalMinutes / (24 * 60);
+const Days2Rounded = Math.floor(Days2);
+let rest1 = TotalMinutes - (Days2Rounded * 24 * 60);
+
+const Hours2 = Math.floor(rest1 / 60);
+let rest2 = rest1 - Hours2 * 60;
+
+const Minutes2 = Math.floor(rest2);
+let rest3 = rest2 - Minutes2;
+
+let Seconds2 = Math.floor(rest3 * 60);
+
+
+printOut("6,322.52 minutes equal to " + Days2Rounded + " days, " + Hours2 + " hours, " + Minutes2 + " minutes, and " + Seconds2 + " seconds.");
 printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Convert 54 dollars to Norwegian kroner, and print the price for both:
+NOK → USD and USD → NOK.
+Use 76 NOK = 8.6 USD as the exchange rate.
+The answer must be in whole "Kroner" and whole "dollars".*/
+
+const NOKtoUSD = 8.6 / 76;
+const USDtoNOK = 76 / 8.6;
+let dollars = 54;
+let NOK = Math.round(dollars / NOKtoUSD);
+printOut("54 USD is equal to " + NOK + " Norwegian kroner.");
+
+dollars = 100;
+NOK = Math.round(dollars / NOKtoUSD);
+printOut("100 USD is equal to " + NOK + " Norwegian kroner.");
+
+let kroner = 100;
+let USD = Math.round(kroner / USDtoNOK);
+printOut("100 Norwegian kroner is equal to " + USD + " USD.");
+
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Create a variable that contains the following text:
+"There is much between heaven and earth that we do not understand."
+● Print the number of characters in the text.
+● Print the character at position number 19.
+● Print the characters starting at position number 35 and 8 characters forward.
+● Print the index at which "earth" starts in the text.*/
+
+const text = "There is much between heaven and earth that we do not understand.";
+printOut("The text has " + text.length + " characters.");
+printOut("The character at position number 19 is: " + text.charAt(19));
+printOut("The characters starting at position number 35 and 8 characters forward are: " + text.charAt(35) + text.charAt(36) + text.charAt(37) + text.charAt(38) + text.charAt(39) + text.charAt(40) + text.charAt(41) + text.charAt(42));
+printOut("The index at which 'earth' starts in the text is: " + text.indexOf("earth"));
+
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Comparison, print the values for the following expressions (evaluate whether the statements are true):
+● Is 5 greater than 3?
+● Is 7 greater than or equal to 7?
+● Is "a" greater than "b"?
+● Is "1" less than "a"?
+● Is "2500" less than "abcd"?
+● "arne" is not equal to "thomas".
+● (2 equals 5) is this statement true?
+● ("abcd" is greater than "bcd") is this statement false?*/
+
+printOut("Is 5 greater than 3?: " + (5 > 3));
+printOut("Is 7 greater than or equal to 7?: " + (7 >= 7));
+printOut('Is "a" greater than "b"?: ' + ("a" > "b"));
+printOut('Is "1" less than "a"?: ' + ("1" < "a"));
+printOut('Is "2500" less than "abcd"?: ' + ("2500" < "abcd"));
+printOut('"arne" is not equal to "thomas": ' + ("arne" != "thomas"));
+printOut("(2 equals 5) is this statement true?: " + (2 === 5));
+printOut('("abcd" is greater than "bcd") is this statement false?: ' + ("abcd" > "bcd"));
+
+/*Gjør denne oppgaven med comparison operators*/
+
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const p8t1 = "254";
+const p8t2 = "57.23";
+const p8t3 = "25 kroner";
+
+printOut(parseFloat(p8t1));
+printOut(parseFloat(p8t2));
+printOut(parseFloat(p8t3));
+
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+let r = Math.ceil(Math.random() * 360);
+printOut("Random number between 1 and 360: " + r);
+
 printOut(newLine);
 
-/* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+let Dager = 131;
+let LeftoverDager = Dager % 7;
+let Uker = (Dager - LeftoverDager) / 7;
+
+printOut("131 days is " + Uker + " weeks and " + LeftoverDager + " days.");
 printOut(newLine);
+
--- module-3/task_3-1/task_3-1.mjs
+++ module-3/task_3-1/task_3-1.mjs
@@ -2,41 +2,138 @@
 import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1, 2, 3 ----------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Task 1, 2 and 3");
-let wakeUpTime = 2;
-if ("Replace this") {
-  printOut("I can take the bus to school.");
+/* ● Part 1
+Complete the given "if" in the task_3-3.mjs file at part 1, statement so that it matches this: If I wake up at
+exactly 7 o'clock then I can catch the bus to school. Run the program with different values of wake-up time
+(6, 7, 8).
+Print out to the HTML page the expression statement you made.
+● Part 2
+Extend part 1 to match this "if" and "else" statement: "If I wake up at exactly 7 o'clock, I can take the bus to
+school, otherwise I have to take the car to school". Run the program with different values of wake-up time
+(6, 7, 8).
+● Part 3
+Extend part 2 to expand more options: “If I wake up at exactly 7 o'clock, I can take the bus to school,
+otherwise if I wake up exactly at 8 o'clock, I can take the train to school, otherwise I have to take the car to
+school”. Run the program with a different value of wake-up time (7, 8).*/
+let wakeUpTime = Math.ceil(Math.random() * 3) + 6;
+if (wakeUpTime == 7)  {
+  printOut("I woke up at " + wakeUpTime +  ", I can take the bus to school.");
 }
+else if (wakeUpTime == 8) {
+  printOut("I woke up at " + wakeUpTime + ", I can take the train to school.");
+}
+else {
+  printOut ("I woke up at " + wakeUpTime + ", I have to take the car to school.");
+}
+
+printOut(newLine);
+printOut("if (wakeUpTime == 7)  {");
+printOut("printOut(\"I woke up at \" + wakeUpTime +  \", I can take the bus to school.\");");
+printOut("}");
+
 printOut(newLine);
 
 printOut("--- Part 4, 5 --------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Write an if statement that checks whether an integer variable is negative or positive, print the text
+"Positive" or "Negative" accordingly. Run the program with different types of values for the variable to
+check the if statement.*/
+
+const P4N1 = 10;
+let P4N2 = Math.ceil(Math.random() * 20);
+let P4N3 = P4N1 - P4N2;
+
+printOut("Is " + P4N3 + " positive, negative or zero?");
+
+if (P4N3 > 0) {
+  printOut("Positive");
+}
+
+else if (P4N3 == 0) {
+  printOut("Zero");
+}
+
+else {
+  printOut("Negative");
+}
+
 printOut(newLine);
 
-printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+printOut("--- Part 6, 7 ----------------------------------------------------------------------------------------------");
+/* Imagine you have a photo editing profession. And you have a website where people can upload pictures
+for you to work on. However, the images must be 4MP or larger, if they are smaller, you cannot use them.
+Create a variable that holds a generated random integer between 1 and 8 (inclusive). Use this variable to
+simulate the uploaded image size and print it. Then create an if statement that prints out “Thank you” if the
+size is equal to or greater than the limit. Otherwise, print out "The image is too small".
+● Part 7
+Expand part 6 to exclude if the image size is larger or equal to 6MP, then print out “Image is too large”.*/
+
+let imageSize = Math.ceil(Math.random() * 8);
+printOut("The image size is: " + imageSize + "MP");
+if (imageSize < 4) {
+  printOut("The image is too small.");
+}
+else if (imageSize >= 6) {
+  printOut("The image is too large.");
+}
+else {
+  printOut("Thank you.");
+}
+
 printOut(newLine);
 
-printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+printOut("--- Part 8, 9 ----------------------------------------------------------------------------------------------");
+/* Use this code:
+const monthList =["January", "February", "Mars", "April", "Mai",
+"Jun", "Juli", "August", "September", "October", "November", "December"];
+const noOfMonth = monthList.length;
+const monthName = monthList[Math.floor(Math.random() * noOfMonth)];
+Print if monthName contains “r”: “You must take vitamin D” else “You do not need to take vitamin D”
+● Part 9
+Expand exercise 8 to print how many days there are in the current month. And do not use date object*/
+
+const monthList =["Januar", "Februar", "Mars", "April", "Mai",
+"Juni", "Juli", "August", "September", "Oktober", "November", "Desember"];
+const noOfMonth = monthList.length;
+const monthName = monthList[Math.floor(Math.random() * noOfMonth)];
+
+printOut("The month is: " + monthName);
+if (monthName.includes("r")) {
+  printOut("You must take vitamin D");
+}
+else {
+  printOut("You do not need to take vitamin D");
+}
+
+let daysInMonth;
+if (monthName === "April" || monthName === "Juni" || monthName === "September" || monthName === "November") {
+  daysInMonth = 30;
+}
+else if (monthName === "Februar") {
+  daysInMonth = 28;
+}
+else {
+  daysInMonth = 31;
+}
+
+printOut(monthName + " has " + daysInMonth + " days.");
 printOut(newLine);
 
-printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
-
-printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
 
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/*● Part 10
+Imagine you have an art gallery, but you need to refurbish the premises, so you close the gallery from
+March through May, but in April you have temporary premises in the building next door. Use the month
+constant in exercise 8 to inform the status of your gallery in that month.*/
+
+if (monthName === "Mars" || monthName === "Mai") {
+  printOut("In the current month of " + monthName + ", the art gallery is closed for refurbishment.");
+}
+else if (monthName === "April") {
+  printOut("In the current month of " + monthName + ", the art gallery is open in temporary premises next door.");
+}
+else {
+  printOut("In the current month of " + monthName + ", the art gallery is open.");
+}
+
 printOut(newLine);
